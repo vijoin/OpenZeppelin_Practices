f81f4420ea846b01ea27f37aac339b7fe7135565
Victor Inojosa <vijoin@gmail.com>
2022-02-12 11:38:45 -0300

    Fix broken test revertedWith breaks when comparing addresses

--- test/RoleTestToken.js
+++ test/RoleTestToken.js
@@ -1,10 +1,7 @@
 const { expect } = require("chai");
-const { keccak256 } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
 describe("AccessControl Contract", function () {
-
-  // const MINTER_ROLE = keccak256("MINTER_ROLE");
 
   let RoleTestToken;
   let roleTestToken;
@@ -12,15 +9,12 @@ describe("AccessControl Contract", function () {
   let addr1;
   let addr2;
   let addrs;
-  let ADMIN_ROLE;
   let MINTER_ROLE;
 
   beforeEach(async function () {
     RoleTestToken = await ethers.getContractFactory("RoleTestToken");
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
     roleTestToken = await RoleTestToken.deploy();
-    await roleTestToken.deployed();
-    ADMIN_ROLE = await roleTestToken.ADMIN_ROLE();
     MINTER_ROLE = await roleTestToken.MINTER_ROLE();
   
   });
@@ -28,36 +22,31 @@ describe("AccessControl Contract", function () {
   describe("Role Assignments", function () {
     it("ADMIN can add Role to an User", async function () {
       await roleTestToken.addRoleToAccount(MINTER_ROLE, addr1.address)
-      expect(await roleTestToken.hasRole(MINTER_ROLE, addr1.address)).to.equal(true);
+      expect(
+        await roleTestToken.hasRole(MINTER_ROLE, addr1.address)
+        ).to.equal(true);
     });
 
     it("ADMIN can remove Role from an User", async function () {
       await roleTestToken.addRoleToAccount(MINTER_ROLE, addr1.address)
       await roleTestToken.removeRoleFromAccount(MINTER_ROLE, addr1.address)
-      expect(await roleTestToken.hasRole(MINTER_ROLE, addr1.address)).to.equal(false);
+      expect(
+        await roleTestToken.hasRole(MINTER_ROLE, addr1.address)
+        ).to.equal(false);
     });
 
     it("non-ADMIN can't add Role from an User", async function () {
-
-      // await roleTestToken.addRoleToAccount(MINTER_ROLE, addr1.address)
-      
       expect(
         roleTestToken.connect(addr2).addRoleToAccount(MINTER_ROLE, addr1.address)
-        ).to.be.revertedWith(
-          `AccessControl: account ${addr2.address} is missing role ${ADMIN_ROLE}`
-        );
-      
-        await roleTestToken.connect(owner);
+        ).to.be.reverted;
     });
 
-    // Can't make it work!!!
-    // it("non-ADMIN can't remove Role from an User", async function () {
-
-    //   await roleTestToken.addRoleToAccount(MINTER_ROLE, addr1.address);
-    //   await roleTestToken.connect(addr2);
-    //   expect(roleTestToken.removeRoleFromAccount(MINTER_ROLE, addr1.address)).to.be.reverted();
-
-    // });
+    it("non-ADMIN can't remove Role from an User", async function () {
+      await roleTestToken.addRoleToAccount(MINTER_ROLE, addr1.address);
+      expect(
+        roleTestToken.connect(addr2).removeRoleFromAccount(MINTER_ROLE, addr1.address)
+        ).to.be.reverted;
+    });
 
   });
 });
