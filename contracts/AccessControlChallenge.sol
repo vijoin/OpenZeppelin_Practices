9adb4a9f5eb82393b885e35a57939ca6fd273310
Victor Inojosa <vijoin@gmail.com>
2022-02-14 10:15:05 -0300

    only ADMIN can add Writers (with unit tests)

--- contracts/AccessControlChallenge.sol
+++ contracts/AccessControlChallenge.sol
@@ -3,12 +3,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 
-contract AccessControlChallenge is Ownable {
+contract AccessControlChallenge is Ownable, AccessControl {
+
+    bytes32 public constant WRITER_ROLE = keccak256("WRITER_ROLE");
 
     constructor () {
-
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
 }
--- test/Challenge1_AccessControlChallenge.js
+++ test/Challenge1_AccessControlChallenge.js
@@ -4,19 +4,24 @@ const { ethers } = require("hardhat");
 describe ("Challenge #1 Access Control", function () {
 
     let owner;
+    let addr1;
+    let addr2;
     let AccessControlChallenge;
     let accessControlChallenge;
+    let WRITER_ROLE;
 
     beforeEach(async function () {
-        [owner, ...addrs] = await ethers.getSigners();
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
         AccessControlChallenge = await ethers.getContractFactory("AccessControlChallenge")
         accessControlChallenge = await AccessControlChallenge.deploy()
+
+        WRITER_ROLE = await accessControlChallenge.WRITER_ROLE();
     });
 
     describe("Contract deployment", function () {
 
         it("Contract was Created successfully", async function () {
-            expect(
+            await expect(
                 await accessControlChallenge.owner()
             ).to.be.equal(owner.address);
         });
@@ -25,8 +30,17 @@ describe ("Challenge #1 Access Control", function () {
 
     describe("Roles", function () {
 
-        it("only ADMIN can add Writers", async function () {
-            expect(false).to.be.equal(true);
+        it("ADMIN can add Writers", async function () {
+            await accessControlChallenge.grantRole(WRITER_ROLE, addr1.address);
+            await expect(
+                await accessControlChallenge.hasRole(WRITER_ROLE, addr1.address)
+            ).to.be.equal(true);
+        });
+
+        it("non-ADMIN can't add Writers", async function () {
+            await expect(
+                accessControlChallenge.connect(addr1).grantRole(WRITER_ROLE, addr2.address)
+            ).to.be.reverted;
         });
 
     });
@@ -34,15 +48,15 @@ describe ("Challenge #1 Access Control", function () {
     describe("Operations", function () {
 
         it("store function call with Writer role", async function () {
-            expect(false).to.be.equal(true);
+            await expect(false).to.be.equal(true);
         });
 
         it("reverted transaction when non-Writer calls store function", async function () {
-            expect(false).to.be.equal(true);
+            await expect(false).to.be.equal(true);
         });
 
         it("receive function call by several roles", async function () {
-            expect(false).to.be.equal(true);
+            await expect(false).to.be.equal(true);
         });
     });
 
